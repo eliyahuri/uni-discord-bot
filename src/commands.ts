a5e7c3ff6d265cba3dda186d0725ff6c4ebe0243
eliyahuri <tkhvu1236@gmail.com>
2024-12-22 17:25:45 +0200

    enhance error handling and logging for command refresh process

--- src/commands.ts
+++ src/commands.ts
@@ -20,14 +20,19 @@ const rest = new REST({ version: "10" }).setToken(config.TOKEN);
 const main = async () => {
     try {
         console.info("Started refreshing application (/) commands.");
-
-        await rest.put(Routes.applicationCommands(config.CLIENT_ID), {
-            body: commands,
-        });
-
-        console.info("Successfully reloaded application (/) commands.");
+        const data = await rest.put(
+            Routes.applicationCommands(config.CLIENT_ID),
+            {
+                body: commands,
+            },
+        );
+        console.info("Successfully reloaded application (/) commands:", data);
     } catch (error) {
-        console.error(error);
+        if (error instanceof Error && "response" in error) {
+            console.error("API Error:", (error as any).response.data);
+        } else {
+            console.error("Unexpected Error:", error);
+        }
     }
 };
 
