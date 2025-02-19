c54b0c0080ec1d6231a1fcea55d8a26565c3717c
eliyahuri <tkhvu1236@gmail.com>
2025-02-19 21:40:26 +0200

    feat: add health check to Dockerfile and enhance TypeScript configuration

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,9 @@ RUN pnpm run build
 # Expose the port (if needed)
 EXPOSE 8080
 
+# Add a health check
+HEALTHCHECK --interval=30s --timeout=10s --start-period=5s --retries=3 \
+  CMD curl -f http://localhost:8080/health || exit 1
+
 # Run the application
 CMD ["node", "dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { client } from "./config/client";
 import config from "./config/config";
 import { commandHandlers } from "./functions/commandFunc";
 
-client.on(Events.ClientReady, (readyClient) => {
-    console.info(`Logged in as ${readyClient.user.tag}!`);
+client.once(Events.ClientReady, () => {
+    console.info(`Logged in as ${client.user?.tag}!`);
 });
+
 client.on(
-    "voiceStateUpdate",
+    Events.VoiceStateUpdate,
     async (oldState: VoiceState, newState: VoiceState) => {
-        // Check if the bot is in a voice channel
         const botId = client.user?.id;
         if (!botId) return;
 
@@ -18,7 +18,6 @@ client.on(
             oldState.guild.voiceStates.cache.get(botId)?.channel;
         if (!botVoiceChannel) return;
 
-        // If the user disconnects and the bot is alone, disconnect the bot
         if (
             oldState.channelId === botVoiceChannel.id &&
             newState.channelId !== botVoiceChannel.id
@@ -27,8 +26,7 @@ client.on(
                 (member) => !member.user.bot,
             );
             if (membersInChannel.size === 0) {
-                const connection = getVoiceConnection(oldState.guild.id);
-                connection?.destroy(); // Disconnect from the voice channel
+                getVoiceConnection(oldState.guild.id)?.destroy();
                 console.log(
                     "Bot disconnected from the voice channel as it was left alone.",
                 );
@@ -38,17 +36,13 @@ client.on(
 );
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    // Only handle Chat Input commands (slash commands)
     if (!interaction.isChatInputCommand()) return;
 
     console.info(
         `Received interaction ${interaction.commandName} from ${interaction.user.tag}`,
     );
 
-    // Retrieve the appropriate handler from the map
     const handler = commandHandlers[interaction.commandName];
-
-    // Call the handler if it exists; otherwise respond with an error or ignore
     if (handler) {
         await handler(interaction);
     } else {
